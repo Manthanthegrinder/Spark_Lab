56bb900f741474170fe598c7e6bf03c274e41c7a
harsh266 <32129648+harsh266@users.noreply.github.com>
2025-05-28 16:39:49 -0600

    Sign in page UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminDashboard from "./components/AdminDashboard";
 import LocationsPage from "./pages/LocationsPage";
 import EventsPage from "./pages/EventsPage";
+import SignInPage from "./pages/SignInPage";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<div>Hello</div>} />
+        <Route path="/signin" element={<SignInPage />} />
         <Route path="/admin" element={<AdminDashboard />}>
           <Route path="locations" element={<LocationsPage />} />
           <Route path="events" element={<EventsPage />} />
